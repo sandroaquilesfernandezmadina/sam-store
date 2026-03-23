8436112b5cc0042ccc43c3d0c128a37c3abb46e6
sandro <fsandroaquiles@gmail.com>
2026-03-23 17:17:04 -0500

    actualizacion de la base de datos

---
+++ category-service/src/main/java/org/sam/controller/CategoryController.java
@@ -0,0 +1,46 @@
+package org.sam.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.sam.dto.CategoryRequest;
+import org.sam.dto.CategoryResponse;
+import org.sam.service.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/categoriy")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    ResponseEntity<List<CategoryResponse>> findAll(){
+        return ResponseEntity.ok(categoryService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity <CategoryResponse> findById(@PathVariable Long id){
+        return ResponseEntity.ok(categoryService.findById(id));
+    }
+
+    @PostMapping
+    ResponseEntity <CategoryResponse> save(@RequestBody CategoryRequest request){
+        return ResponseEntity.ok(categoryService.save(request));
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity <CategoryResponse> update(@PathVariable Long id, @RequestBody CategoryRequest request){
+        return ResponseEntity.ok(categoryService.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity <Void> delete(@PathVariable Long id){
+        categoryService.findById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
--- category-service/src/main/java/org/sam/mapper/CategoryMapper.java
+++ category-service/src/main/java/org/sam/mapper/CategoryMapper.java
@@ -24,5 +24,4 @@ public class CategoryMapper {
             category.getDescription()
         );
     }
-
 }
